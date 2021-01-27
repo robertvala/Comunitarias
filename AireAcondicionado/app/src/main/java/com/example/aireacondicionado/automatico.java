e4c417072c3200cf690e43f871adecf284c055bf
robertvala <robavala@espol.edu.ec>
2021-01-26 21:17:12 -0500

    Automatico

--- AireAcondicionado/app/src/main/java/com/example/aireacondicionado/automatico.java
+++ AireAcondicionado/app/src/main/java/com/example/aireacondicionado/automatico.java
@@ -7,6 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +32,12 @@ public class automatico extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    EditText editTextInicio,editTextFinal,editTextTemperatura;
+    TextView txtrepeticion;
+    Spinner spRepetecion;
+    Button btnEnviarAuto;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Control").child("Automatico");
 
     public automatico() {
         // Required empty public constructor
@@ -59,6 +74,44 @@ public class automatico extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_automatico, container, false);
+        View root=inflater.inflate(R.layout.fragment_automatico, container, false);
+        editTextFinal=root.findViewById(R.id.editTextFinal);
+        editTextInicio=root.findViewById(R.id.editTextInicio);
+        editTextTemperatura=root.findViewById(R.id.editTextControl);
+        btnEnviarAuto=root.findViewById(R.id.btnEnviarAuto);
+        txtrepeticion=root.findViewById(R.id.txtRepeticion);
+        spRepetecion=root.findViewById(R.id.spRepeticion);
+        String [] opciones={"Diariamente","Una sola vez"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line,opciones);
+        spRepetecion.setAdapter(adapter);
+        String repeticion=spRepetecion.getSelectedItem().toString();
+        txtrepeticion.setText(repeticion);
+        enviar();
+        return root;
+    }
+
+    void enviar(){
+        btnEnviarAuto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inicio=editTextInicio.getText().toString();
+                String fin=editTextFinal.getText().toString();
+                String temperatura=editTextTemperatura.getText().toString();
+
+                myRef.child("HoraInicial").setValue(inicio);
+                myRef.child("HoraFinal").setValue(fin);
+                myRef.child("TemperaturaControl").setValue(temperatura);
+
+                String repeticion=spRepetecion.getSelectedItem().toString();
+                txtrepeticion.setText(repeticion);
+
+                myRef.child("Repeticion").setValue(repeticion);
+
+
+                Toast.makeText(getContext(), "Enviado con exito", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
     }
 }
